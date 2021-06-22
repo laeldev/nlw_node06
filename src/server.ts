5a3bda5647957cceec61768f7876567efe93caaf
Andre Lael <andrelael.contato@gmail.com>
2021-06-22 16:20:36 -0400

    criado migrations e entity

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,7 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
 const app = express();
-
-app.get("/test", (request, response)=> {
-  
-  response.send("Ola NLW, Metodo GET");
-});
-
-app.post("/test-post", (request, response)=> {
-
-  response.send("Ola NLW, Metodo POST");
-});
 
 app.listen(3000, () => console.log("Servidor Rodando"));
